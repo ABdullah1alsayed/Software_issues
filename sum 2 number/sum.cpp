99a34b2702b5ae2210fb50f832148e015278a01e
ABdullah1alsayed <51753417+ABdullah1alsayed@users.noreply.github.com>
2021-07-01 15:49:03 +0200

    add new folder and create file c++ for example to train the git

---
+++ sum 2 number/sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+  int x , c, sum;
+  cout << "enter the tow number :"<<endl;
+  cin >> x;
+  cin >> c ;
+   sum = x+ c ;
+   cout  << " the sum is  "<<sum << endl;
+   return 0 ;
+}
